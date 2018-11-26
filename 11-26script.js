982c47f8ab109190cd70bf38cbb20c0158c307c0
Jacob Bennett <kf7ags@gmail.com>
2018-11-26 09:47:37 -0700

    added draw card promise

--- 11-26script.js
+++ 11-26script.js
@@ -1,10 +1,25 @@
 
-let ourPromise = async ()=>{
+let ourPromise = ()=>{
     return new Promise((resolve,reject)=>{
         $ajax({
             url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",
             type: "GET",
             success: (response, status)=>{
+                resolve(response);
+            },
+            error: (error)=>{
+                reject(error);
+            }
+        });
+    })
+};
+
+let cardPromise = (deckID)=>{
+    return new Promise((resolve,reject)=>{
+        $ajax({
+            url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`,
+            type: "GET",
+            success: (response,status)=>{
                 resolve(response);
             },
             error: (error)=>{
